7ff675299a51845fa84c8e49a1124d31832400e3
adehndr <adehendra@outlook.com>
2023-12-29 11:10:52 +0700

    feat: session

---
+++ session/page1.php
@@ -0,0 +1,8 @@
+<?php 
+  // don't forget to invoke this function if 
+  // you neet to manipulate a session
+  session_start();
+
+  // put the variable in session
+  $_SESSION["name"] = "Ade Hendra";
+?>
---
+++ session/page2.php
@@ -0,0 +1,5 @@
+<?php
+  session_start();
+  // access the field
+  echo $_SESSION["name"];
+?>
